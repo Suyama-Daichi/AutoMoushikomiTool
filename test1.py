031dd211e9792ee7157b3a26bec79eea86b0c819
SuyamaDaichi <symdit@gmail.com>
2020-01-20 22:32:46 +0900

    ログイン切り替え機能

--- test1.py
+++ test1.py
@@ -26,60 +26,61 @@ maisu = 2
 # いつでもよいを選択するか(Trueならいつでもよい)
 itsudemoOK = True
 
+memberIds = [
+    "00886551"
+    ]
+password = ["luna1213"]
 
 
 
+for memberId in memberIds:
+    # 1.操作するブラウザを開く
+    driver = webdriver.Chrome('chromedriver.exe')
 
+    # 2.操作するページを開く
+    driver.get('https://www.fc-member.johnnys-net.jp/login/index/f/JI')
 
+    # ログインIDを入力する
+    driver.find_element_by_id("member_id").send_keys(memberId)
 
+    # パスワードを入力する
+    driver.find_element_by_id("password").send_keys(password[0])
 
-# 1.操作するブラウザを開く
-driver = webdriver.Chrome('chromedriver.exe')
-
-# 2.操作するページを開く
-driver.get('https://www.fc-member.johnnys-net.jp/login/index/f/JI')
-
-# ログインIDを入力する
-driver.find_element_by_id("member_id").send_keys("00886551")
-
-# パスワードを入力する
-driver.find_element_by_id("password").send_keys("luna1213")
-
-# ログインボタンをクリックする
-driver.find_element_by_xpath("//*/section/form/div[2]/div/button").click()
-
-for koenNo in koenNos:
-    # 指定の公演の申し込みページを開く
-    driver.get(koenUrl)
-
-    # チェックボックスにチェックを入れる
-    driver.find_element_by_class_name("checkbox").click()
-
-    # 申し込み本ページに遷移する
-    driver.find_element_by_id("applicant-link").click()
-
-    # 第一希望を選択する
-    Select(driver.find_element_by_id("request1")).select_by_value(koenNo)
-
-    # 第四希望を選択する()
-    Select(driver.find_element_by_id("request4")).select_by_index(1 if itsudemoOK else 2)
-
-    # 次へボタンをクリック
+    # ログインボタンをクリックする
     driver.find_element_by_xpath("//*/section/form/div[2]/div/button").click()
 
-    # チケット枚数を選択
-    Select(driver.find_element_by_id("qty")).select_by_index(maisu)
+    for koenNo in koenNos:
+        # 指定の公演の申し込みページを開く
+        driver.get(koenUrl)
 
-    # 次へボタンをクリック
-    driver.find_element_by_xpath(
-        "//*/section/form/div[2]/div[1]/button").click()
+        # チェックボックスにチェックを入れる
+        driver.find_element_by_class_name("checkbox").click()
 
-    time.sleep(2)
+        # 申し込み本ページに遷移する
+        driver.find_element_by_id("applicant-link").click()
 
-    # 申し込むボタンをクリック
-    driver.find_element_by_xpath(
-        "//*/section/form/div[2]/div[1]/button").click()
+        # 第一希望を選択する
+        Select(driver.find_element_by_id("request1")).select_by_value(koenNo)
 
-    time.sleep(2)
+        # 第四希望を選択する()
+        Select(driver.find_element_by_id("request4")).select_by_index(1 if itsudemoOK else 2)
+
+        # 次へボタンをクリック
+        driver.find_element_by_xpath("//*/section/form/div[2]/div/button").click()
+
+        # チケット枚数を選択
+        Select(driver.find_element_by_id("qty")).select_by_index(maisu)
+
+        # 次へボタンをクリック
+        driver.find_element_by_xpath(
+            "//*/section/form/div[2]/div[1]/button").click()
+
+        time.sleep(2)
+
+        # 申し込むボタンをクリック
+        driver.find_element_by_xpath(
+            "//*/section/form/div[2]/div[1]/button").click()
+
+        time.sleep(2)
 else:
     print("申し込み完了！")
